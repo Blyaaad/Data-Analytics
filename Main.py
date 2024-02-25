d9ea106ba102a116c58b290894dcbab22e602838
EROLEROLEROLEROL <113877955+EROLEROLEROLEROL@users.noreply.github.com>
2024-02-25 21:14:16 +0800

    bar graph

--- Main.py
+++ Main.py
@@ -10,37 +10,38 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 1000)
 print(cyber_sec_atk.head(1001))
 
-column_name = input("What column would you want in the table? (Protocol, "
+#Single Variable tables and charts
+#Frequncy distribution table
+freq_column = input("What column would you want in the table? (Protocol, "
                    "Packet Type, Traffic Type, Malware Indicators, Anomaly Scores, "
                    "Attack Type, Attack Signature, Action Taken, Severity, Device Information, "
                    "Network Segment, Firewall Logs, IDS/IPS Alerts, Log Source)")
-
-#Single Variable tables and charts
-#Frequncy distribution table
-frequency_table = cyber_sec_atk[column_name].value_counts().reset_index()
+frequency_table = cyber_sec_atk[freq_column].value_counts().reset_index()
 frequency_table.columns = ['Value', 'Frequency']
-
 # Sort the table based on the values (optional)
 frequency_table = frequency_table.sort_values(by='Value')
-
 print(frequency_table)
 
-
-'''
 #Bar chart
 #Change the 'x_column' and 'y_column' to the columns that we want to plot
-x_column = 'x_column'
-y_column = 'y_column'
+# Set display options to show all columns and limit rows
+# Prompt the user to input the column name
+column_name = input("Enter the column name for which you want to create a frequency distribution: ")
 
-# Group by the x column and sum the y column
-grouped = cyber_sec_atk.groupby(x_column)[y_column].sum()
-
+# Group by the x column and count the occurrences
+grouped = cyber_sec_atk.groupby(column_name).size()
 # Plotting
 # Change the labels to the appropriate terms
-grouped.plot(kind='bar')
-plt.xlabel(x_column)
-plt.ylabel('Sum of ' + y_column)
-plt.title('Bar Chart of ' + y_column + ' Grouped by ' + x_column)
+ax = grouped.plot(kind='bar')
+plt.xlabel(column_name)
+plt.ylabel('Frequency')
+plt.title('Bar Chart of Frequency Grouped by ' + column_name)
+
+# Add labels on top of each bar
+for i, freq in enumerate(grouped):
+    ax.text(i, freq + 0.05, str(freq), ha='center', va='bottom')
+
 plt.show()
-'''
+
+
 
